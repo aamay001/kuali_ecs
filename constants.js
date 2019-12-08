25d65ae11dbaaacb27ee6496b08ba1c5694d8c1a
Andy Amaya <aamay001@gmail.com>
2019-12-08 09:56:02 -0800

    Constants file.

---
+++ constants.js
@@ -0,0 +1,15 @@
+export const DOOR_STATUS = {
+  OPEN: true,
+  CLOSED: false,
+};
+
+export const ELEVATOR_STATUS = {
+  IN_USE: true,
+  VACANT: false,
+};
+
+export const ELEVATOR_DIRECTION = {
+  UP: 2,
+  DOWN: 1,
+  STOPPED: 0,
+};
