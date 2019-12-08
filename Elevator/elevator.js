e6facaf1487da9f82a5f0d6e3fd67f2643b3222b
Andy Amaya <aamay001@gmail.com>
2019-12-08 10:37:00 -0800

    Elevator methods and exporting.

--- Elevator/elevator.js
+++ Elevator/elevator.js
@@ -1,24 +1,28 @@
-import {
+const {
   DOOR_STATUS,
   ELEVATOR_DIRECTION,
   ELEVATOR_STATUS,
-} from '../constants';
+} = require('../constants');
 
 /**
  * @name Elevator
  * @description Elevator class.
+ * @param {string} elevatorId Unique identifier
+ * @param {number} numFloors Total number of floors in elevator system.
+ * @param {number} startFloor Starting floor.
  */
 class Elevator {
   constructor(elevatorId, numFloors, startFloor = 1) {
     this.id = elevatorId;
-    this.doorState = DOOR_STATUS.CLOSED;
-    this.status = ELEVATOR_STATUS.VACANT;
     this.direction = ELEVATOR_DIRECTION.UP;
     this.trips = 0;
     this.floorsPassed = 0;
     this.stoppedForMaintenance = false;
     this.maxFloor = numFloors;
     this.currentFloor = startFloor;
+    this.reportFloor();
+    this.openDoor();
+    this.setStatus(ELEVATOR_STATUS.VACANT);
   }
 
   reportFloor() {
@@ -38,12 +42,19 @@ class Elevator {
 
   setDirection(direction) {
     this.direction = direction;
+    let desc = 'stopped';
+    if (direction === ELEVATOR_DIRECTION.UP) {
+      desc = 'going up';
+    } else if (direction === ELEVATOR_DIRECTION.DOWN) {
+      desc = 'going down';
+    }
+    console.log(`${this.id} : Elevator is ${desc}`);
   }
 
   logTrip(from, to, timeStart, timeEnd) {
     this.trips++;
     // If logging to a db, include all meta
-    if (trips >= 0) {
+    if (this.trips >= 0) {
       this.stoppedForMaintenance = true;
     }
   }
@@ -71,12 +82,25 @@ class Elevator {
     this.openDoor();
   }
 
-    moveToFloor(floor) {
-      if (floor > this.currentFloor) {
-        this.currentFloor = Math.max(1, this.currentFloor - 1);
-      } else if (floor > this.currentFloor) {
-        this.currentFloor = Math.min(this.maxFloor, this.currentFloor + 1);
-      }
-      this.reportFloor();
+  moveToFloor(floor) {
+    if (floor < this.currentFloor) {
+      this.currentFloor = Math.max(1, this.currentFloor - 1);
+    } else if (floor > this.currentFloor) {
+      this.currentFloor = Math.min(this.maxFloor, this.currentFloor + 1);
     }
+    this.reportFloor();
+  }
+
+  setStatus(status) {
+    this.status = status;
+    console.log(`${this.id} : Elevator ${status ? 'in use' : 'vacant'}`);
+  }
+
+  userRequest(floor) {
+    this.setStatus(ELEVATOR_STATUS.IN_USE);
+    this.goToFloor(floor);
+    this.setStatus(ELEVATOR_STATUS.VACANT);
+  }
 }
+
+module.exports = Elevator;
--- constants.js
+++ constants.js
@@ -1,15 +1,21 @@
-export const DOOR_STATUS = {
+const DOOR_STATUS = {
   OPEN: true,
   CLOSED: false,
 };
 
-export const ELEVATOR_STATUS = {
+const ELEVATOR_STATUS = {
   IN_USE: true,
   VACANT: false,
 };
 
-export const ELEVATOR_DIRECTION = {
+const ELEVATOR_DIRECTION = {
   UP: 2,
   DOWN: 1,
   STOPPED: 0,
+};
+
+module.exports = {
+  ELEVATOR_DIRECTION,
+  ELEVATOR_STATUS,
+  DOOR_STATUS,
 };
